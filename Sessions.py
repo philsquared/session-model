01ee2f9eae890c8b6313573a74dc6b41050aadc0
Phil Nash <phil.nash@github.com>
2024-01-06 21:49:05 +0000

    Added load_yaml

--- Sessions.py
+++ Sessions.py
@@ -13,21 +13,34 @@ def _sessions_to_dict(sessions: [Session]) -> list:
     return [asdict(session, dict_factory=null_filter_factory) for session in sessions]
 
 
-def save_sessions(sessions: [Session], filename: str):
+def load_yaml(filename: str) -> dict:
+    """
+    Reads a YAML file into a dictionary
+    """
+    with codecs.open(filename, 'r', "utf-8") as f:
+        return yaml.safe_load(f)
+
+
+def save_yaml(filename: str, data: dict):
+    """
+    Writes a dictionary to a YAML file
+    """
+    with codecs.open(filename, "w", "utf-8") as f:
+        yaml.dump(data, f, default_flow_style=False)
+
+
+def save_sessions(filename: str, sessions: [Session]):
     """
     Writes an array of sessions to a YAML file
     """
     data = _sessions_to_dict(sessions)
-    with codecs.open(filename, "w", "utf-8") as f:
-        yaml.dump(data, f, default_flow_style=False)
+    save_yaml(filename, data)
 
 
 def load_sessions(filename: str) -> [Session]:
     """
     Reads an array of sessions from a YAML file
     """
-    with codecs.open(filename, 'r', "utf-8") as f:
-        data = yaml.safe_load(f)
-
+    data = load_yaml(filename)
     sessions = [dict_to_object(session_data, Session) for session_data in data]
     return sessions
