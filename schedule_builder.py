db8b9a77aef5d5f91c5966bf7b7a54873b75e557
Phil Nash <github@philnash.me>
2024-02-11 21:36:08 +0000

    Moved schedule builder in

---
+++ schedule_builder.py
@@ -0,0 +1,163 @@
+import yaml
+
+from sessionmodel.schedule_model import Session, Timeslot, Schedule, Day, SessionSlot, Time, WorkshopGroup
+from pykyll.markdown import render_markdown
+from pykyll.utils import format_longdate
+from sessionmodel.Sessions import load_sessions
+
+
+schedule = None
+session_by_id = {}
+session_by_slug = {}
+
+
+class ScheduleBuilder:
+    def __init__(self, session_data_by_id: dict):
+        self.session_data_by_id = session_data_by_id
+
+    def read_session_slot(self, index: int, session_slot_entry, times: [Time], live: bool) -> SessionSlot:
+        def make_session(session_id: str, times: [Time]) -> Session:
+            session_info = self.session_data_by_id.get(session_id)
+            if not session_info:
+                raise Exception(f"No session data for {session_id}")
+            session = Session(
+                id=session_id,
+                live=live,
+                data=session_info,
+                start_time=times[0],
+                end_time=times[1])
+            if session_id not in session_by_id:
+                session_by_id[session_id] = session
+                slug = session.title_as_slug
+                if slug in session_by_slug:
+                    raise Exception(f"Two sessions have the same slug, '{slug}'")
+                session_by_slug[slug] = session
+            else:
+                session = session_by_id[session_id]
+            return session
+
+        if isinstance(session_slot_entry, dict):
+            # explicit session_slot (alt start/ end times or multiple sessions)
+            session_slot_data = session_slot_entry.get("session_slot")
+            if not session_slot_data:
+                raise Exception(f"Only 'session_slot' key currently supported, but found: {session_slot_entry.keys()}")
+
+            sessions = [make_session(session_entry["session"], session_entry["time"]) for session_entry in session_slot_data]
+            return SessionSlot(index, sessions=sessions)
+        else:
+            # implicit session_slot - just one, full-length, session
+            return SessionSlot(index, sessions=[make_session(session_slot_entry, times)])
+
+    def read_session_slots(self, session_slot_data, times: [Time], live_data: [int]) -> [SessionSlot]:
+        return [self.read_session_slot(index, data, times, live == 1) for index, (data, live) in enumerate(zip(session_slot_data, live_data))]
+
+    def read_timeslots(self, timeslot_data: [dict]) -> [Timeslot]:
+        timeslots = []
+        for time_num, data in enumerate(timeslot_data):
+            sessions_data = data["sessions"]
+            times = data["time"]
+            live_data = data.get("live") or [0 for _ in sessions_data]
+            session_slots = self.read_session_slots(sessions_data, times, live_data)
+            times = set(times)
+            for rs in session_slots:
+                for s in rs.sessions:
+                    times.add(s.start_time)
+                    times.add(s.end_time)
+            times = list(times)
+            times.sort()
+            for rs in session_slots:
+                for s in rs.sessions:
+                    s.start_time_index = times.index(s.start_time)
+                    s.end_time_index = times.index(s.end_time)
+            timeslot = Timeslot(
+                times=times,
+                type=data.get("type") or "sessions",
+                session_slots=session_slots)
+
+            timeslots.append(timeslot)
+        return timeslots
+
+    def read_days(self, day_data) -> [Day]:
+        days = []
+        for data in day_data:
+            date = data["date"]
+            day_num = data["day_num"]
+            day = Day(
+                rooms=data["rooms"],
+                day_num=day_num,
+                day=data["day"],
+                date=date,
+                date_str=format_longdate(date),
+                date_components=[date.year, date.month-1, date.day],
+                type=data.get("type") or "normal",
+                label=data.get("label") or "",
+                alt_label=data.get("alt_label") or "",
+                timeslots = self.read_timeslots(data["timeslots"]))
+
+            for timeslot in day.timeslots:
+                session_count = len(timeslot.session_slots)
+                room_count = len(day.rooms)
+                if session_count > 1 and session_count != room_count:
+                    raise Exception(f"Mismatch between number of rooms on {day.day} ({room_count}) and number of sessions at {timeslot.times[0]} ({session_count})")
+
+            days.append(day)
+        return days
+
+
+def load_schedule(schedule_path: str, session_data_path: str, fixed_session_data_path: str, workshops_only=False) -> Schedule:
+    sessions = load_sessions(session_data_path)
+
+    if fixed_session_data_path is not None:
+        fixed_sessions = load_sessions(fixed_session_data_path)
+        sessions = sessions + fixed_sessions
+
+    for session in sessions:
+        session.title = render_markdown(
+            session.title,
+            clean=True,
+            strip_outer_p_tag=True,
+            embedded_code=True,
+            remove_elements=["h1", "h2", "h3"])
+
+    session_data_by_id = {session.id: session for session in sessions}
+    builder = ScheduleBuilder(session_data_by_id)
+
+    with open(schedule_path, 'r') as f:
+        data = yaml.safe_load(f)
+
+    if workshops_only:
+        days = data["days"]
+        workshop_days = []
+        for day in days:
+            if "workshop" in day["label"]:
+                workshop_days.append(day)
+        data["days"] = workshop_days
+
+
+    global schedule
+    schedule = Schedule(
+        room_names=data["room_names"],
+        default_header=data["default_header"],
+        days=builder.read_days(data["days"]))
+
+    workshops_seen = set()
+    workshops = []
+    for day in schedule.days:
+        for timeslot in day.timeslots:
+            for session_slots in timeslot.session_slots:
+                for session in session_slots.sessions:
+                    session.day.append(day)
+                    if session.is_workshop:
+                        if session.id not in workshops_seen:
+                            workshops_seen.add(session.id)
+                            workshops.append(session)
+
+    for workshop in workshops:
+        date_range = workshop.date_range
+        if not schedule.workshop_groups or schedule.workshop_groups[-1].date_range != date_range:
+            group = WorkshopGroup(workshop.day[0].alt_label, date_range, [])
+            schedule.workshop_groups.append(group)
+        else:
+            group = schedule.workshop_groups[-1]
+        group.workshops.append(workshop)
+
---
+++ schedule_model.py
@@ -0,0 +1,228 @@
+import datetime
+import os
+from dataclasses import dataclass, field
+from functools import cached_property
+
+from glassware.logging import log_warn
+from pykyll.html import slugify, make_description
+from pykyll.markdown import render_markdown
+from pykyll.utils import common_suffix
+from sessionmodel import Session as SessionModel
+
+class Time:
+    def __init__(self, time_str: str):
+        parts = time_str.split(":")
+        if len(parts) != 2:
+            raise Exception(f"Invalid time: `{time_str}`")
+        self.hour = int(parts[0])
+        self.min = int(parts[1])
+
+    @property
+    def as_key(self):
+        return f"{self.hour}_{self.min}"
+
+    @property
+    def total_minutes(self):
+        return self.min * 60 + self.hour
+
+    def __str__(self):
+        return f"{self.hour}:{self.min}"
+
+    def __repr__(self):
+        return f"{self.hour}:{self.min}"
+
+    def __lt__(self, other):
+        return (self.hour < other.hour or
+                (self.hour == other.hour) and self.min < other.min)
+
+
+@dataclass
+class Speaker:
+    data: SessionModel.Speaker
+
+    @property
+    def bio_as_html(self):
+        return render_markdown(self.data.bio, clean=True, strip_outer_p_tag=True)
+
+    @property
+    def profile_pic_path(self):
+        # !TBD: extract the year
+        if self.data.profile_pic:
+            return os.path.join("/static", "img", "profiles", "2024", self.data.profile_pic )
+        else:
+            return None
+
+    @property
+    def header_image_path(self):
+        # !TBD: extract the year
+        if self.data.header_image:
+            return os.path.join("/static", "img", "profiles", "2024", self.data.header_image )
+        else:
+            return None
+
+
+@dataclass()
+class Session:
+    id: str
+    live: bool
+    data: SessionModel.Session
+
+    start_time: Time
+    end_time: Time
+
+    # Indices into Timeslot times
+    start_time_index = 0
+    end_time_index = -1
+
+    day: list = field(default_factory=list)  # Set after init
+
+    @property
+    def is_workshop(self) -> bool:
+        return self.data.type == "workshop"
+
+    @property
+    def duration_in_minutes(self):
+        return self.end_time.total_minutes - self.start_time.total_minutes
+
+    @property
+    def length_description(self):
+        if self.data.length.isnumeric():
+            return f"{self.data.length} minute session"
+        else:
+            return self.data.length
+
+    @property
+    def speaker_names(self):
+        names = [s.name for s in self.data.speakers]
+        match len(names):
+            case 0:
+                return ""
+            case 1:
+                return names[0]
+            case 2:
+                return f"{names[0]} & {names[1]}"
+            case _:
+                return ", ".join(names[0:-2]) + f" & {names[-1]}"
+
+    @property
+    def title_with_names(self):
+        if self.data.speakers:
+            return f"{self.data.title} - {self.speaker_names}"
+        else:
+            return self.data.title
+
+    @property
+    def title_as_html(self) -> str:
+        return render_markdown(self.data.title, clean=True, strip_outer_p_tag=True)
+
+    @property
+    def title_as_slug(self):
+        return slugify(self.data.title)
+
+    @property
+    def abstract_as_html(self) -> str:
+        return render_markdown(self.data.abstract, linkify=True, clean=True, strip_outer_p_tag=True)
+
+    @property
+    def outline_as_html(self) -> str:
+        return render_markdown(self.data.outline, linkify=True, clean=True, strip_outer_p_tag=True)
+
+    @property
+    def short_abstract_as_html(self) -> str:
+        html = render_markdown(self.data.abstract, linkify=True, clean=True, strip_outer_p_tag=True)
+        return make_description(html)
+
+    @property
+    def speakers(self):
+        return [Speaker(s) for s in self.data.speakers]
+
+    @property
+    def header_image(self):
+        image = None
+        for speaker in self.speakers:
+            if speaker.header_image_path is not None:
+                if image is None:
+                    image = os.path.join("/static", "img", "profiles", "2024", speaker.header_image_path)
+                else:
+                    log_warn("Multiple speakers have header images - selecting the first one")
+        return image
+
+    @property
+    def date_range(self):
+        if len(self.day) == 1:
+            return f"{self.day[0].day}, {self.day[0].date_str}"
+        elif len(self.day) > 1:
+            date1 = self.day[0].date_str
+            date2 = self.day[-1].date_str
+            date1_parts = date1.split(" ")
+            date2_parts = date2.split(" ")
+            suffix = common_suffix(date1_parts, date2_parts)
+            distinct_len = len(date1_parts) - len(suffix)
+            date1 = " ".join(date1_parts[:distinct_len])
+            date2 = " ".join(date2_parts[:distinct_len])
+            date_common = " ".join(date1_parts[distinct_len:])
+            return f"{self.day[0].day}, {date1} - {self.day[1].day}, {date2} {date_common}"
+        else:
+            raise Exception(f"Session, {self.title_as_slug} has no Day field")
+
+
+# A timeslot for a room - usually  just one session, but may be multiple
+@dataclass
+class SessionSlot:
+    index: int
+    sessions: [Session]
+
+    @property
+    def is_single(self):
+        return len(self.sessions) == 1
+
+
+@dataclass
+class Timeslot:
+    times: [Time]
+    type: str
+    session_slots: [SessionSlot]
+
+    @cached_property
+    def has_speakers(self):
+        for r in self.session_slots:
+            for s in r.session_slots:
+                if s.data.speakers:
+                    return True
+        return False
+
+    @property
+    def is_trackless(self):
+        return len(self.session_slots) == 1
+
+
+@dataclass
+class Day:
+    rooms: [int]
+
+    # Day/ date in various formats
+    day_num: int
+    day: str
+    date: datetime.date
+    date_components: [int]
+    date_str: str
+
+    type: str
+    label: str
+    alt_label: str
+    timeslots: [Timeslot]
+
+
+@dataclass
+class WorkshopGroup:
+    name: str
+    date_range: str
+    workshops: [Session]
+
+
+@dataclass
+class Schedule:
+    room_names: [str]
+    default_header: str
+    days: [Day]
+    workshop_groups: [WorkshopGroup] = field(default_factory=list)
