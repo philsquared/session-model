c3af6640ac6a8e16c10847a9590c57314d5450a5
Phil Nash <github@philnash.me>
2025-04-09 18:06:34 +0100

    Allow more fields to be defaulted on Speaker

--- Speaker.py
+++ Speaker.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from sessionmodel.Colour import Colour
 from sessionmodel.Link import Link
@@ -8,11 +8,11 @@ from sessionmodel.Link import Link
 class Speaker:
     id: int
     name: str
-    friendly_name: str
-    bio: str
+    friendly_name: str = ""
+    bio: str = ""
 
     # These fields may be missing initially
-    links: [Link]   # Social media/ website/ blog etc
+    links: [Link] = field(default_factory=list)   # Social media/ website/ blog etc
     profile_pic: str | None = None
     header_image: str | None = None
     tint_colour: Colour | None = None
